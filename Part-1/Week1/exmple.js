17e9d67aef5fad3ab0a99f90100f2fc6478b0afd
Ivona Ivkovic <iivkovic@my.bcit.ca>
2022-09-16 12:15:38 -0700

    adding more code

--- Part-1/Week1/exmple.js
+++ Part-1/Week1/exmple.js
@@ -2,3 +2,28 @@
 // declre a variable called car
 
 var car;
+// camelCase
+// variable called bugs collected and initialize it to value 4
+var bugsCollected = 4;
+
+// PascalCase
+// variable call bugs collected and initialize it to value 4;
+var BugsCollected = 4;
+
+// SnakeCase
+// variable called bugs collected and initialize to value 4;
+var Bugs_Collected = 4;
+// Initialize the variable electronics box to the word cpu.
+// Use camelCase
+var electronicsBox = "cpu";
+// Boolean
+// Initilize the variable called proceed to the value tru.
+var proceed = true;
+var i = 0;
+
+while(proceed) {
+    i++;
+    proceed = false;
+}
+console.log(i);
+
